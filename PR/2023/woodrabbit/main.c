48fb5f6b26a9c4ad9be0ce1e650933b660e62b3c
woodrabbit <woodrabbit@qq.com>
2023-07-20 17:56:08 +0800

    Initialize the PR directory and compose the first program
    These things have been done:
    1. Create a PR directory.
    2. Create a woodrabbit(my_gitee_id) directory.
    3. This program main.c was written in woodrabbit(my_gitee_id) name.
    
    Signed-off-by: woodrabbit <woodrabbit@qq.com>

---
+++ PR/2023/woodrabbit/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Please fill in your own Gitee ID here. */
+char *pMyGiteeId = "woodrabbit";
+
+int main(void) {
+    printf("Greetings form %s!\n", pMyGiteeId);
+    return 0;
+}
+
