ec6b7900295acb775b6a7e2d2e6f7a6d0cfc7399
woodrabbit <woodrabbit@qq.com>
2023-07-22 15:35:28 +0800

    Redesign the source file and directory structure
    1. Redesign the source file main.c
    2. The source files are placed in the src directory, and the documentation is placed in the docs directory. Files with the file name set to Gitee ID will be placed in the directory above src.
    
    Signed-off-by: woodrabbit <woodrabbit@qq.com>

---
+++ PR/2023/src/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    DIR *dir = opendir("..");
+    if (dir == NULL) {
+        perror("Unable to open parent directory");
+        return 1;
+    }
+
+    struct dirent *entry;
+
+    while ((entry = readdir(dir)) != NULL) {
+        // 只处理常规文件（排除目录、软链接等）。
+        // 另外，排除“.”和“..”目录项。并且排除
+        // 带有扩展名的文件（其中包含“.”），因为
+        // 该程序要求将文件名设置成Gitee ID，因此
+        // 其中不可能包含“.”。
+        if (entry->d_type == DT_REG && 
+            strchr(entry->d_name, '.') == NULL) {
+            printf("Greetings from %s\n", entry->d_name);
+        }
+    }
+
+    closedir(dir);
+
+    printf(":)\n");
+    return 0;
+}
