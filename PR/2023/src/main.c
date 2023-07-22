5ed2f33b284258617d7b4d0a89883e65defb6ffb
woodrabbit <woodrabbit@qq.com>
2023-07-22 16:52:39 +0800

    Add comments and updated the printing information for the program
    Signed-off-by: woodrabbit <woodrabbit@qq.com>

--- PR/2023/src/main.c
+++ PR/2023/src/main.c
@@ -15,14 +15,16 @@ int main() {
     struct dirent *entry;
 
     while ((entry = readdir(dir)) != NULL) {
-        // 只处理常规文件（排除目录、软链接等）。
-        // 另外，排除“.”和“..”目录项。并且排除
-        // 带有扩展名的文件（其中包含“.”），因为
-        // 该程序要求将文件名设置成Gitee ID，因此
-        // 其中不可能包含“.”。
+        // 1. 只处理常规文件（排除目录、软链接等）。
+        // 2. 排除“.”和“..”目录项，并排除带有扩展名的文件
+        //    （其中包含“.”）。根据Gitee ID命名规则，其中
+        //    不可能包含“.”。
+        // 3. Gitee ID命名规则：只允许字母、数字或者下划线
+        //    （_）、中划线（-），至少 2 个字符，必须
+        //    以字母开头，不能以特殊字符结尾。
         if (entry->d_type == DT_REG && 
             strchr(entry->d_name, '.') == NULL) {
-            printf("Greetings from %s\n", entry->d_name);
+            printf("Greetings from %s!\n", entry->d_name);
         }
     }
 
